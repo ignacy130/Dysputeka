9c8e5398725a8d4487e3eed639e171feeaf5e54e
Tomasz Marchewka <o2.tomek@tlen.pl>
2015-01-05 21:27:19 +0100

    model odpowiedzi

--- Dysputeka.Core/Domain/Entities/Answer.cs
+++ Dysputeka.Core/Domain/Entities/Answer.cs
@@ -9,6 +9,12 @@ namespace Dysputeka.Core.Domain.Entities
 {
     public class Answer : Entity
     {
-
+        public virtual string Content { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual DateTime EditDate { get; set; }
+        public virtual Rate Rate { get; set; }
+        public virtual Vote Vote { get; set; }
+        public virtual User User { get; set; }
+        public virtual Question Question { get; set; }
     }
 }
